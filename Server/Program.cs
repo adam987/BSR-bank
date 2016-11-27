216e0b08605b31441ccdb939e292d3d50baf8333
Adam Szczepaniak <adam987@users.noreply.github.com>
2016-11-27 18:55:04 +0100

    initial commit: project structure

---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ServiceModel;
+using Server.RestServices;
+using Server.SoapServices;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var soapService = new ServiceHost(typeof(SoapService)))
+            {
+                using (var restService = new ServiceHost(typeof(RestService)))
+                {
+                    Console.WriteLine("Starting soap service");
+                    soapService.Open();
+                    Console.WriteLine("Starting rest service");
+                    restService.Open();
+                    Console.WriteLine("Press ENTER to stop server...");
+                    Console.ReadLine();
+                    Console.WriteLine("Stopping server");
+                    soapService.Close();
+                }
+            }
+        }
+    }
+}
