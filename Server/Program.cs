314ccaa6e6a32d6b8b2a6d7798ccd7cdf945b1be
Adam Szczepaniak <adam987@users.noreply.github.com>
2016-11-27 19:32:07 +0100

    start services

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel;
 using Server.RestServices;
 using Server.SoapServices;
@@ -7,22 +9,23 @@ namespace Server
 {
     internal class Program
     {
+        private static readonly List<Type> Services = new List<Type> {typeof(SoapService), typeof(RestService)};
+
         private static void Main(string[] args)
         {
-            using (var soapService = new ServiceHost(typeof(SoapService)))
+            var serviceHosts = Services.Select(service =>
             {
-                using (var restService = new ServiceHost(typeof(RestService)))
-                {
-                    Console.WriteLine("Starting soap service");
-                    soapService.Open();
-                    Console.WriteLine("Starting rest service");
-                    restService.Open();
-                    Console.WriteLine("Press ENTER to stop server...");
-                    Console.ReadLine();
-                    Console.WriteLine("Stopping server");
-                    soapService.Close();
-                }
-            }
+                Console.WriteLine($"Starting {service.Name}");
+                var serviceHost = new ServiceHost(service);
+                serviceHost.Open();
+                return serviceHost;
+            }).ToList();
+
+            Console.WriteLine("Press ENTER to stop server...");
+            Console.ReadLine();
+            Console.WriteLine("Stopping server");
+
+            serviceHosts.ForEach(serviceHost => serviceHost.Close());
         }
     }
 }
